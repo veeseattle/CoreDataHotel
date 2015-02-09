58a41382a0b3e3f9fd48d9a13428592222ad9501
Vania Kurniawati <vania.baek@gmail.com>
2015-02-09 13:07:03 -0800

    Initial Commit

---
+++ CoreDataHotel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataHotel
+//
+//  Created by Vania Kurniawati on 2/9/15.
+//  Copyright (c) 2015 Vania Kurniawati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
